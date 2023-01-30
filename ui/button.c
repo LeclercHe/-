a288d5207bb10201d38844add5ec9f19b469da6e
LeclercHe <Leclerce@qq.com>
2023-01-30 17:03:38 +0800

    first commit

---
+++ ui/button.c
@@ -0,0 +1,43 @@
+
+#include "ui.h"
+
+static int DefaultOnDraw(struct Button *ptButton, PDispBuff ptDispBuff)
+{
+    /*绘制底色*/
+    DrawRegion(&ptButton->tRegion, BUTTON_DEFAULT_COLOR);
+    /*居中显示文字*/
+    SetFontSize(ptButton->iFontSize);
+    DrawTextInRegionCentral(ptButton->name, &ptButton->tRegion, BUTTON_TEXT_COLOR);
+    /*刷到LCD/Web*/    
+    FlushDisplayRegion(&ptButton->tRegion, ptDispBuff);
+
+    return 0;
+}
+static int DefaultOnPressed(struct Button *ptButton, PDispBuff ptDispBuff, PInputEvent ptInputEvent)
+{
+    unsigned int dwCorlor = BUTTON_DEFAULT_COLOR;
+    
+    ptButton->status = !ptButton->status;
+    if(ptButton->status)
+       dwCorlor = BUTTON_PRESSED_COLOR;
+    /*绘制底色*/
+    DrawRegion(&ptButton->tRegion, dwCorlor);
+    /*居中显示文字*/
+    DrawTextInRegionCentral(ptButton->name, &ptButton->tRegion, BUTTON_TEXT_COLOR);
+    /*刷到LCD/Web*/    
+    FlushDisplayRegion(&ptButton->tRegion, ptDispBuff);
+
+    return 0;
+}
+
+
+
+void InitButton(PButton ptButton, char *name, PRegion ptRegion , ONDRAW_FUNC OnDraw, ONPRESSED_FUNC OnPressed)
+{
+    ptButton->status    = 0;
+    ptButton->name      = name;
+    if(ptRegion)
+        ptButton->tRegion   = *ptRegion;
+    ptButton->OnDraw    = OnDraw ? OnDraw : DefaultOnDraw;
+    ptButton->OnPressed = OnPressed ? OnPressed : DefaultOnPressed;
+}
